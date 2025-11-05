5eaca2e0aa8e98d4c359ae9b4a15d0f6df8847fa
Monu.Alam <132586354+MonuAlam@users.noreply.github.com>
2025-11-05 22:00:03 +0530

    5 Nov task (#8)
    * Test github action (#1)
    
    * first test commit
    
    * first test commit
    
    * update action file
    
    * syntax fixed
    
    * Test github action (#1) (#3) (#4)
    
    * first test commit
    
    * first test commit
    
    * update action file
    
    * syntax fixed
    
    Co-authored-by: monualamEdu <monualam.edu@gmail.com>
    
    * Gfg/pattern 1 (#6)
    
    * first test commit
    
    * pattern question solve
    
    ---------
    
    Co-authored-by: Monu Alam <monualam.edu@gmail.com>
    
    ---------
    
    Co-authored-by: monualamEdu <monualam.edu@gmail.com>

---
+++ src/pattern/GFG3.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class GFG3 {
+
+    static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void main(String[] args) {
+
+        pattern(5);
+
+    }
+}
