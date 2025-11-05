83ed00927a5c10a0f2b0940c174ecc15a8c71d4b
Monu Alam <monualam.edu@gmail.com>
2025-11-05 21:43:43 +0530

    pattern question solve

---
+++ src/pattern/GFG4.java
@@ -0,0 +1,27 @@
+package pattern;
+
+public class GFG4 {
+
+//            1
+//            2 2
+//            3 3 3
+//            4 4 4 4
+//            5 5 5 5 5
+//            6 6 6 6 6 6
+
+    static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void main(String[] args) {
+
+        pattern(5);
+
+    }
+}
+
