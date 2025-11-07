d8dcf476c50a81d4519452fc45bb74791acf6b3b
Monu.Alam <132586354+MonuAlam@users.noreply.github.com>
2025-11-07 09:42:48 +0530

    Gfg/pattern 1 (#9)
    * first test commit
    
    * pattern question solve
    
    * 6 Nov task
    
    ---------
    
    Co-authored-by: Monu Alam <monualam.edu@gmail.com>

---
+++ src/pattern/GFG8.java
@@ -0,0 +1,23 @@
+package pattern;
+
+public class GFG8 {
+
+
+    static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j=0;j<2*n-(2*i+1);j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void main(String[] args) {
+
+        pattern(6);
+
+    }
+}
